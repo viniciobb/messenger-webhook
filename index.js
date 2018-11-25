efa362dee56596bd7570754018cc28af338ce3d7
viniciobb <viniciobb@gmail.com>
2018-11-25 19:30:38 -0200

    callSendAPI - correcao token

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ function callSendAPI(sender_psid, response) {
   // Send the HTTP request to the Messenger Platform
   request({
     "uri": "https://graph.facebook.com/v2.6/me/messages",
-    "qs": { "access_token": EAAEbkIH10nMBALMzQAk8poEdrVT70GjZC1q4ZClwSSZCh1OVweiU4RZAmwMjpltQwmHol4ZB9ZCSieZCK9EC04CpAZCueqSNSZBkkFvD4iechYr9BksdisQZCgkGQeBNsH64GPZBg0AHG2tSzPIp9KGDv7fbXGjyf0UzZAjQoYgarsVFavDXP6G2aXkp4UjPzYm1vrZCX4OZADvHdRhAZDZD },
+    "qs": { "access_token": "EAAEbkIH10nMBALMzQAk8poEdrVT70GjZC1q4ZClwSSZCh1OVweiU4RZAmwMjpltQwmHol4ZB9ZCSieZCK9EC04CpAZCueqSNSZBkkFvD4iechYr9BksdisQZCgkGQeBNsH64GPZBg0AHG2tSzPIp9KGDv7fbXGjyf0UzZAjQoYgarsVFavDXP6G2aXkp4UjPzYm1vrZCX4OZADvHdRhAZDZD" },
     "method": "POST",
     "json": request_body
   }, (err, res, body) => {
