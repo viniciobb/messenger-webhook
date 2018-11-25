64a474b10bc38f6d3762f12ef7aa38fb859b1089
viniciobb <viniciobb@gmail.com>
2018-11-25 14:13:54 -0200

    persistindo resposta do wit

--- index.js
+++ index.js
@@ -3,11 +3,15 @@
 // Imports dependencies and set up http server
 const
   express = require('express'),
+  fetch = require('node-fetch'),
   bodyParser = require('body-parser'),
   app = express().use(bodyParser.json()); // creates express http server
+  
   var MongoClient = require('mongodb').MongoClient;
   var url = "mongodb://vinicioMongo:123mongo@cluster0-shard-00-00-lsstx.mongodb.net:27017,cluster0-shard-00-01-lsstx.mongodb.net:27017,cluster0-shard-00-02-lsstx.mongodb.net:27017/BancarioBot?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
-
+  //var CLIENT_TOKEN = '4TMZFCK5FZUGFDFIRXRJNVY4Y4SEMRDZ'; // TCC-BANCARIO
+  var CLIENT_TOKEN = 'CFPCUH3V443ESVXTNEGTZSPHHRXNHEBX'; // BANCARIO
+  
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
@@ -25,17 +29,44 @@ app.post('/webhook', (req, res) => {
         // Gets the message. entry.messaging is an array, but 
         // will only ever contain one message, so we get index 0
         let webhook_event = entry.messaging[0];
+        console.log("webhook_event");
         console.log(webhook_event);
+        console.log("webhook_event.message");
+        console.log(webhook_event.message);
+      
 
-        MongoClient.connect(url, function(err, db) {
-          if (err) throw err;
-          var dbo = db.db("BancarioBot");
-          dbo.collection("Message").insertOne(webhook_event, function(err, res) {
-            if (err) throw err;
-            console.log("1 document inserted");
-            db.close();
-          });
-        });
+        const q = encodeURIComponent(webhook_event.message);
+        const uri = 'https://api.wit.ai/message?q=' + q;
+        const auth = 'Bearer ' + CLIENT_TOKEN;
+        fetch(uri, {headers: {Authorization: auth}})
+        .then(res => res.json())
+        .then(res => {
+          
+              console.dir(res);
+              
+              console.log(res._text);
+              console.log(res.entities);
+
+              MongoClient.connect(url, function(err, db) {
+                if (err) throw err;
+                var dbo = db.db("BancarioBot");
+
+                var myobj = { 
+                  webhook_event: webhook_event, 
+                  res: res
+                };
+
+                dbo.collection("Message").insertOne(myobj, function(err, res) {
+                  if (err) throw err;
+                  console.log("1 document inserted");
+                  db.close();
+                });
+              });
+                         
+        
+          }
+      
+        );
 
       });
   
