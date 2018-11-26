49e37d8a00739bfab021ae52806041dc85c45149
viniciobb <viniciobb@gmail.com>
2018-11-26 18:59:44 -0200

    envio de resposta do wit para o bot

--- index.js
+++ index.js
@@ -109,15 +109,49 @@ function handleMessage(sender_psid, received_message) {
   if (received_message.text) {    
 
     // Create the payload for a basic text message
-    response = {
-      "text": `You sent the message: "${received_message.text}". Now send me an image!`
-    }
-  }  
+    //response = {
+      //"text": `You sent the message: "${received_message.text}". Now send me an image!`
+    //}
 
-  console.log("chamou callSendAPI" );
+    const q = encodeURIComponent(received_message.message.text);
+    const uri = 'https://api.wit.ai/message?q=' + q;
+    const auth = 'Bearer ' + CLIENT_TOKEN;
+    fetch(uri, {headers: {Authorization: auth}})
+    .then(res => res.json())
+    .then(res => {
+      
+          console.dir(res);
+          console.log(res._text);
+          console.log(res.entities);
+
+          console.log("chamou callSendAPI" );
+
+          // Sends the response message
+          callSendAPI(sender_psid, res);
+
+          // MongoClient.connect(url, function(err, db) {
+          //   if (err) throw err;
+          //   var dbo = db.db("BancarioBot");
+
+          //   var myobj = { 
+          //     sender_psid: sender_psid,
+          //     webhook_event: webhook_event, 
+          //     respostaWIT: res
+          //   };
+
+          //   dbo.collection("Message").insertOne(myobj, function(err, res) {
+          //     if (err) throw err;
+          //     console.log("1 document inserted");
+          //     db.close();
+          //   });
+          // });
+                      
+    
+      }
   
-  // Sends the response message
-  callSendAPI(sender_psid, response);
+    );
+
+  }  
 
 }
 
